785f1ea67237662c32874f30dd900d4d94d6defb
Josh Reeves <josh@plyinteractive.com>
2015-05-22 11:29:12 -0700

    Upgrade to latest published bower version

--- bower_postinstall.sh
+++ bower_postinstall.sh
@@ -2,7 +2,7 @@
 
 mv -f bower_components/cedar/js/* lib/assets/javascripts
 
-mv -f bower_components/cedar/css/cedar.css lib/assets/stylesheets/cedar_source.css
+mv -f bower_components/cedar/css/cedar.css lib/assets/stylesheets/cedar_source.scss
 
 mv -f bower_components/cedar/img/* lib/assets/images
 
