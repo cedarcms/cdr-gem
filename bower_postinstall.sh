3d5e90038587d41afd688bd06a03a142e3a95d99
Josh Reeves <josh@plyinteractive.com>
2015-06-22 11:02:38 -0700

    Update assets to latest bower package and restructure files to support new JS config file in the gem

--- bower_postinstall.sh
+++ bower_postinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-mv -f bower_components/cedar/js/* lib/assets/javascripts
+mv -f bower_components/cedar/js/cedar.js lib/assets/javascripts/cedar_source.js
+
+mv -f bower_components/cedar/js/cedar_handlebars.js lib/assets/javascripts/cedar_handlebars.js
 
 mv -f bower_components/cedar/css/cedar.css lib/assets/stylesheets/cedar_source.scss
 
