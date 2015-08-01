15d84626360f7b7e6b4d731e43a150884d5901ef
Josh Reeves <josh@plyinteractive.com>
2015-08-01 11:24:59 -0700

    Force reload of cedar config when js file is reloaded

--- lib/cedar/init.rb
+++ lib/cedar/init.rb
@@ -5,4 +5,11 @@ class Sprockets::DirectiveProcessor
     rescue
     end
   end
+
+  def process_load_cedar_initializer_directive
+    begin
+      load("#{Rails.root}/config/initializers/cedar.rb")
+    rescue
+    end
+  end
 end
